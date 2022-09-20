13ea1e60f98ae1a94494cd398a2698b892b2c705
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-09-20 12:52:55 +0545

    changed the api function

--- index.js
+++ index.js
@@ -8,18 +8,17 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
-  const count = 5;
-
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  )
+app.get('/api/animals', (req, res) => {
+const animals = [
+'donkey',
+'tiger',
+'lion'
+]
 
   // Return them as json
-  res.json(passwords);
+  res.json(animals);
 
-  console.log(`Sent ${count} passwords`);
+  console.log(animals);
 });
 
 // The "catchall" handler: for any request that doesn't
