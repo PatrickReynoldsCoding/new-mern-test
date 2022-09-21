0f7d9c585358d5e91a6b83a9a0b5f7ace8ce8f03
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-09-21 13:45:48 +0545

    create function added to controller

--- controllers/animals.js
+++ controllers/animals.js
@@ -1,15 +1,24 @@
+const Animal = require('../models/animals')
+
 const AnimalController = {
 
+  create: async (req, res) => {
+    const animal = new Animal({
+    name: req.body.name,
+    
+    });
+ 
+
+
+    
+
+    await animal.save().then((doc) => res.status(201).json(animal)); //doc?
+
+  },
 
   mammals: async (req, res) => {
-    const animals = [
-      "dog",
-      "cat",
-      "whale"
-    ]
-
-    res.json(animals)
-
+    const allAnimals = await Animal.find();
+    return res.json(allAnimals);
   }
 
 }
--- routes/animals.js
+++ routes/animals.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const AnimalController = require('../controllers/animals');
 
 router.get('/mammals', AnimalController.mammals);
+router.post('/createmammal', AnimalController.create);
 
 
 module.exports = router;
